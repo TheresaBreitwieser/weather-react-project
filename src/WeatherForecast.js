32ab441d0b84f45b2959c15da94d3f6b676d74be
TheresaBreitwieser <theresabreitwieser@gmail.com>
2021-10-13 08:45:24 +0200

    Addition of return valure for .map method for display of daily forecast
    React warning - Array Callback return fixed by adding a return value of null for the .map method to show daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -25,7 +25,9 @@ export default function WeatherForecast(props) {
                         </div>
                         );
                     }
-                    })}   
+                    else{
+                        return(null);
+                    }})}   
                  </div>
             </div>
         );
